0099038802d8a8419e428e2b07c3a7ff49b6a054
sabaemi <saba.em9@gmail.com>
2022-11-01 16:07:15 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This program is to show join optimization, which has the ability to determine the order of joining two or more tables. In this implementation, we suppose the cost of executing each join is equal to the product of the number of rows in the first table multiplied by the ceiling divided by the number of rows in the second table by 20, i.e.
+cost(i,j)=No. of rows of T_i *⌈((No. of rows of T_j)/20)⌉.
+Also, we suppose that when we join two tables together, the result will be a new temporary table whose number of rows is equal to the product of the number of rows of the two joined tables.
+The input of the program is the total number of tables and the number of rows in each table.
+The program works for each input and finally determines the order of the joins and transfers this order to the output along with the total cost.
---
+++ program/AdvancedDatabaseHW3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AdvancedDatabaseHW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of tables :");
+            int nt = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[nt];
+            int[] order = new int[nt];
+            char[] name = new char[nt];
+            char ch= 'A';
+            for (int i = 0; i < nt; i++)
+            {
+                name[i] = ch;
+                Console.Write("Enter rows of table "+ name[i]+ " : ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                ch++;
+            }
+
+            Array.Sort(arr, name);
+            order = arr;
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Oder of Tables : ");
+            for (int i = 0; i < nt; i++)
+            {
+                Console.Write(name[i] + " : ");
+                Console.WriteLine(order[i] + " ");
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Optimal Oder : ");
+            for (int i = 0; i < nt - 2; i++) Console.Write("<");
+            Console.Write(name[0]+" Join "+ name[1]);
+            for (int i = 2; i < nt; i++) Console.Write("> Join "+ name[i]);
+
+            Console.WriteLine("");
+
+            int Sum = 0;
+            int PriorRowCount = order[0];
+
+            for (int i = 1; i <= nt - 1; i++)
+            {
+                Sum += PriorRowCount * Convert.ToInt32(Math.Ceiling((double)order[i] / 20));
+                PriorRowCount *= order[i];
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Optimal Cost : " + Sum);
+        }
+    }
+}
